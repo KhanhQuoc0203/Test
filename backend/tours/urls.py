321567a6cd83de4261ab2cfa9044e31cbb8e90d5
KhanhQuoc <quockhanh032005@gmail.com>
2026-04-13 01:44:40 +0700

    Day 4 cua Khanh

--- backend/tours/urls.py
+++ backend/tours/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TourCreateView
+from .views import TourCreateView, TourDetailAPIView
 
 urlpatterns = [
     path('', TourCreateView.as_view(), name='tour-create'),
+    path('<int:pk>/', TourDetailAPIView.as_view(), name='tour-detail'),
 ]
